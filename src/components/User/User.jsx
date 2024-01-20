548386de8e1ca6c28f53cd45ba0020365b775af1
Anrisichinava922 <anrisichinava922@gmail.com>
2024-01-21 00:04:55 +0400

    CardAPP

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -5,26 +5,26 @@ const User = () => {
   return (
     <form>
         <div className='user-card-details'>
-        <div className="cardholder-name">
-            <h1 className='chn-h1'>Cardholder Name</h1>
-            <input className='ch-input' type="text"  placeholder='NAME SURNAME'/>
-        </div>
-        <div className="card-number">
-            <h2 className='cn-h2'>Card Number</h2>
-            <input className='cn-input' type="text"  placeholder='ENTER YOUR CARD NUMBER'/>
-        </div>
-        <div className="card-date">
-            <h3 className='exp-h3'>Exp. Date (MM/YY)   CVC</h3>
-            <div className="expiration-date">
-                <input className='month' type="text" placeholder='MM'/>
-                <input className='year' type="text" placeholder='YY'/>
-                <input className='digitalcode' type="text" placeholder='3 DGN'/>
+            <div className="cardholder-name">
+                <h1 className='user-h1'>Cardholder Name</h1>
+                <input className='ch-input' type="text"  placeholder='NAME SURNAME'/>
             </div>
-            <div className="btn">
-                <button>SUBMIT</button>
+            <div className="card-number">
+                <h2 className='user-h2'>Card Number</h2>
+                <input className='cn-input' type="text"  placeholder='ENTER YOUR CARD NUMBER'/>
+            </div>
+            <div className="card-date">
+                <h3 className='user-h3'>Exp. Date (MM/YY)   CVC</h3>
+                <div className="expiration-date">
+                    <input className='month' type="text" placeholder='MM'/>
+                    <input className='year' type="text" placeholder='YY'/>
+                    <input className='digitalcode' type="text" placeholder='3 DGN'/>
+                </div>
+                <div className="btn">
+                    <button>CONFIRM</button>
+                </div>
             </div>
         </div>
-    </div>
     </form>
     
   )
